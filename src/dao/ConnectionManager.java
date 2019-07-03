bccd60552650d0a66a24c2fb2b218efbf1c5e0b1
Ske63 <ske63.20@gmail.com>
2019-07-03 19:47:38 +0900

    2019/07/03 ConnectionManagerを追加
    + ConnectionManager.javaを追加

---
+++ src/dao/ConnectionManager.java
@@ -0,0 +1,26 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionManager
+{
+    /** データベースアクセス用URL */
+    private static final String URL;
+
+    /** データベースアクセス用ユーザ名 */
+    private static final String USER;
+
+    /** データベースアクセス用パスワード */
+    private static final String PASSWORD;
+
+    /**
+     * データベースのConnectionを取得する。
+     * @return コネクション
+     */
+    public Connection getConnection () throws SQLException
+    {
+        return DriverManager.getConnection ( URL, USER, PASSWORD );
+    }
+}
