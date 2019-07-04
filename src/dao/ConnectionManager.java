5cbd94120ef970146fa686c0048c400710635184
Ske63 <ske63.20@gmail.com>
2019-07-04 23:36:55 +0900

    2019/07/04 【動作未確認】ConnectionManager.javaにコンストラクタを追加

--- src/dao/ConnectionManager.java
+++ src/dao/ConnectionManager.java
@@ -18,6 +18,19 @@ public class ConnectionManager
     /** データベースアクセス用パスワード */
     private static final String PASSWORD;
 
+	/**
+	 * データベースアクセスの設定を行う。
+	 * @param データベースアクセス用URL
+	 * @param データベースアクセス用ユーザ名
+	 * @param データベースアクセス用パスワード
+	 */
+	public ConnectionManager ( String url, String user, String password )
+	{
+		URL = url;
+		USER = user;
+		PASSWORD = password;
+	}
+
     /**
      * データベースのConnectionを取得する。
      * @return コネクション
