4c3872a0cdc45c0d57e92a0279cfbb443976d71b
Ske63 <ske63.20@gmail.com>
2019-07-07 17:19:56 +0900

    2019/07/07 【動作確認済】ユーザ登録情報重複確認処理はUserInfoDAOを使用するように変更
    DBへのユーザの登録処理時に使用する、
    ユーザ登録情報の重複確認処理に使用するメソッドを下記に変更
    変更箇所：UserLogic.execute(User)
    変更前：RegisterSelect.select(User)
    変更後：UserInfoDAO.selectUserInfoByMail(String)

--- src/logic/UserLogic.java
+++ src/logic/UserLogic.java
@@ -1,22 +1,47 @@
 package logic;
 
+import java.sql.SQLException;
 import java.util.regex.Pattern;
 
-import dao.RegisterSelect;
+//import dao.RegisterSelect;
+import dao.ConnectionManager;
+import dao.UserInfoDAO;
 import entity.User;
+import entity.UserInfo;
 
 public class UserLogic {
 	public void execute(User u) {
 		String pass = u.getPass();
 		if(Pattern.matches("(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]{8,}",pass)) {
+/*
 			RegisterSelect rs = new RegisterSelect();
 			try {
 				rs.select(u);
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
+*/
+			// メールアドレスの重複確認
+			UserInfo userInfo = null;
+			ConnectionManager conManager = new ConnectionManager ( "jdbc:mysql://localhost:8889/EC", "admin", "admin" );
+			try
+			{
+				UserInfoDAO userInfoDao = new UserInfoDAO ( conManager.getConnection () );
+				userInfo = userInfoDao.selectUserInfoByMail ( u.getMail () );
+			}
+			catch ( SQLException e )
+			{
+				e.printStackTrace ();
+			}
 
-			u.setIsbool(true);
+			if ( userInfo == null )
+			{
+				u.setIsbool(true);
+			}
+			else
+			{
+				u.setIsbool(false);
+			}
 
 	}else {
 		u.setIsbool(false);
